2ae906d4f26aa2e74cba9b4bfcab9a2646326c56
Ho-Feng <Ho-Feng.Wong@whitevision.nl>
2023-05-08 16:05:26 +0200

    eerste commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import generatePdfThumbnails from 'pdf-thumbnails-generator';
 
 function App() {
+  const [thumbnails, setThumbnails] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const generateThumbnails = async (inputFile) => {
+    setLoading(true);
+    try {
+      const thumbnailsResult = await generatePdfThumbnails(inputFile, 150);
+      setThumbnails(thumbnailsResult);
+    } catch (err) {
+      console.error(err);
+    }
+    setLoading(false);
+  }
+
+  const handleUpload = async (e) => {
+    console.log('Processing ' + e.target.files[0].name)
+
+    const reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+    reader.onloadend = () => {
+      //const base64data = reader.result;
+      const file = reader.result;
+      generateThumbnails(file);
+      //return base64data;
+    };
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section>
+          <input type='file' accept='application/pdf' onChange={handleUpload} />
+
+          <br />
+
+          {
+            thumbnails
+              ? thumbnails.map(({ thumbnail, page }) =>
+                <ImageComp key={page} src={thumbnail} index={page} />)
+              : null
+          }
+
+          {loading && <h1>Thumbnails aan het genereren...</h1>}
+        </section>
       </header>
     </div>
   );
 }
 
 export default App;
+
+const ImageComp = ({ src, index }) => (<div style={{ margin: '1rem', display: 'inline-block' }}>
+  <img src={src} alt={index} />
+  <p>Page Index: {index}</p>
+</div>);
